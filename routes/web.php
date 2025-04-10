68f629501b5fc667a277f3fd691063eadb482183
MD Saiful Islam <saifulmsi454@gmail.com>
2025-04-10 23:03:45 +0600

    admin template setup

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
+});
+
+Route::get('/dashboard', function () {
+    return view('components.CRUD.index');
+    
 });
