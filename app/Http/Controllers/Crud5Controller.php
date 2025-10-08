5113c26920972a7ff9c4cca5026cfd2452aa716a
git-saif <saifse454@gmail.com>
2025-10-09 01:29:53 +0600

    update & delete function created

--- app/Http/Controllers/Crud5Controller.php
+++ app/Http/Controllers/Crud5Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Crud5;
 use Illuminate\Http\Request;
 use App\Http\Requests\Crud5Request;
+use App\Models\Crud;
 
 class Crud5Controller extends Controller
 {
@@ -67,15 +68,42 @@ class Crud5Controller extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $crud5 = Crud5::findOrFail($id);
+        return view('components.CRUD-5.edit', compact('crud5'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Crud5Request $request, string $id)
     {
-        //
+        $crud5 = Crud5::findOrFail($id);
+
+        try{
+            $validated = $request->validated();
+
+            $img_path = $crud5->image;
+            if ($request->hasFile('image')) {
+                if ($crud5->image && file_exists(public_path($crud5->image))) {
+                    unlink(public_path($crud5->image));
+                }
+                $image = $request->file('image');
+                $filename = time() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('uploads/images/crud5'), $filename);
+                $img_path = 'uploads/images/crud5/' . $filename;
+        }
+
+            $crud5->update([
+                'name'  => $validated['name'],
+                'email' => $validated['email'],
+                'phone' => $validated['phone'],
+                'image' => $img_path,
+            ]);
+
+            return redirect()->route('dashboard.crud-5.index')->with('success', 'Data updated successfully!');
+        } catch (\Exception $e) {
+            return back()->with('error', 'Something went wrong: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -83,6 +111,17 @@ class Crud5Controller extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $crud5 = Crud5::findOrFail($id);
+
+            if ($crud5->image && file_exists(public_path($crud5->image))) {
+                unlink(public_path($crud5->image));
+            }
+
+            $crud5->delete();
+            return redirect()->route('dashboard.crud-5.index')->with('success', 'Data deleted successfully!');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', 'Something went wrong: ' . $th->getMessage());
+        }
     }
 }
