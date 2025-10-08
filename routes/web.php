411b27e5c0ab5e300ed053e46227e57b2d2e6aa0
git-saif <saifse454@gmail.com>
2025-10-09 01:33:59 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,8 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
 
     Route::resources([
 
-        'crud-0' => Crud0Controller::class,
-        'crud' => CrudController::class,
-        'crud-2' => Crud2Controller::class,
+        'crud-1' => Crud0Controller::class,
+        'crud-2' => CrudController::class,
         'crud-3' => Crud2Controller::class,
 
         // CRUD With Advance Validation -> app/Http/Requests/FolderName/RequestName.php
