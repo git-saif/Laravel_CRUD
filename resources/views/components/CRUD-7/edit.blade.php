85ba9ea8112ee861dfcdaa6744feb82ab55a4810
git-saif <saifse454@gmail.com>
2025-10-12 00:50:13 +0600

    crud-7 (category) edit & update are created

---
+++ resources/views/components/CRUD-7/edit.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.app')
+
+@section('content')
+@section('title', 'Smart ERP - Edit Entry')
+<!-- Edit form -->
+<div class="main-content">
+  <div class="main-content-inner">
+    <div class="breadcrumbs ace-save-state" id="breadcrumbs">
+      <ul class="breadcrumb">
+        <li><i class="ace-icon fa fa-home home-icon"></i><a href="#">Home</a></li>
+        <li><a href="#">Tables</a></li>
+        <li class="active">Edit Entry</li>
+      </ul>
+    </div>
+
+    <div class="page-content">
+      <div class="page-header">
+        <h1>
+          Edit Entry
+          <small><i class="ace-icon fa fa-angle-double-right"></i> Update Existing Data</small>
+        </h1>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="row">
+            <div class="col-xs-12 ">
+              <div class="col-md-2"></div>
+
+              <div class="col-md-8">
+                <div class="widget-header widget-header-flat " style="background-color: #618f8f;">
+                  <h4 class="widget-title" style="color: #fff;">Edit Data</h4>
+
+                  <span class="widget-toolbar">
+                    <a href="{{ route('dashboard.crud-7.index') }}" style="color: #fff;">
+                      <i class="ace-icon fa fa-list"></i> Back to List
+                    </a>
+                  </span>
+                </div>
+
+                <!-- Edit Form Start -->
+                <form action="{{ route('dashboard.crud-7.update', $crud7->id) }}" method="POST" enctype="multipart/form-data">
+                  @csrf
+                  @method('PUT')
+
+                  {{-- Name --}}
+                  <div class="form-group">
+                    <label for="name">Category Name</label>
+                    <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $crud7->name) }}" placeholder="Enter category name" required>
+                    @error('name')
+                    <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                  </div>
+
+                  {{-- Slug (auto-generated) --}}
+                  <div class="form-group">
+                    <label for="slug">Slug (Auto Generated)</label>
+                    <input type="text" id="slug" name="slug" class="form-control" value="{{ old('slug', $crud7->slug) }}" readonly>
+                  </div>
+
+                  {{-- Serial Number --}}
+                  <div class="form-group">
+                    <label for="serial_no">Serial No</label>
+                    <input type="number" name="serial_no" class="form-control @error('serial_no') is-invalid @enderror" value="{{ old('serial_no', $crud7->serial_no) }}" placeholder="Enter serial number" required>
+                    @error('serial_no')
+                    <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                  </div>
+
+                  {{-- Status --}}
+                  <div class="form-group">
+                    <label for="status">Status</label><br>
+                    <label>
+                      <input type="radio" name="status" value="active" {{ old('status', $crud7->status) == 'active' ? 'checked' : '' }}> Active
+                    </label>
+                    &nbsp;&nbsp;
+                    <label>
+                      <input type="radio" name="status" value="inactive" {{ old('status', $crud7->status) == 'inactive' ? 'checked' : '' }}> Inactive
+                    </label>
+                    @error('status')
+                    <br><small class="text-danger">{{ $message }}</small>
+                    @enderror
+                  </div>
+
+
+
+                  <div class="form-actions center">
+                    <button type="submit" class="btn btn-sm btn-success">
+                      Update
+                      <i class="ace-icon fa fa-check icon-on-right bigger-110"></i>
+                    </button>
+
+                    <a href="{{ route('dashboard.crud-7.index') }}" class="btn btn-sm btn-warning">
+                      <i class="ace-icon fa fa-arrow-left bigger-110"></i> Back
+                    </a>
+                  </div>
+                </form>
+                <!-- Edit Form End -->
+              </div>
+              <div class="col-md-2"></div>
+            </div>
+          </div>
+        </div><!-- /.col -->
+      </div>
+    </div><!-- /.page-content -->
+  </div>
+</div>
+<!-- /.main-content -->
+
+@endsection
+
