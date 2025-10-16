49cf89a39c4e554d133384df015f44ee792845a7
git-saif <saifse454@gmail.com>
2025-10-16 22:16:30 +0600

    update sidebar to dynamic sidebar

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -36,291 +36,122 @@
     </div><!-- /.sidebar-shortcuts -->
 
     <ul class="nav nav-list">
-        <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
-            <a href="{{ route('dashboard') }}">
-                <i class="menu-icon fa fa-tachometer"></i>
-                <span class="menu-text"> Dashboard </span>
+      {{-- Dashboard --}}
+      <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
+        <a href="{{ route('dashboard') }}">
+          <i class="menu-icon fa fa-tachometer"></i>
+          <span class="menu-text"> Dashboard </span>
+        </a>
+        <b class="arrow"></b>
+      </li>
+
+      @php
+      $currentRoute = request()->route()->getName();
+      @endphp
+
+      
+        {{-- CRUD SECTION (1–6) --}}
+      <li class="{{ Str::startsWith($currentRoute, 'dashboard.crud-') && (int)explode('.', explode('-', $currentRoute)[1])[0] <= 6 ? 'active open' : '' }}">
+        <a href="javascript:void(0);" class="dropdown-toggle">
+          <i class="menu-icon fa fa-list"></i>
+          <span class="menu-text"> CRUD </span>
+          <b class="arrow fa fa-angle-down"></b>
+        </a>
+
+        <b class="arrow"></b>
+
+        <ul class="submenu">
+          @for ($i = 1; $i <= 6; $i++) <li class="{{ request()->routeIs('dashboard.crud-' . $i . '.*') ? 'active' : '' }}">
+            <a href="{{ route('dashboard.crud-' . $i . '.index') }}">
+              <i class="menu-icon fa fa-caret-right"></i>
+              CRUD - {{ $i }}
             </a>
-            <b class="arrow"></b>
+      </li>
+      @endfor
+      </ul>
+      </li>
+
+
+    {{-- ==========================
+        BLOG MANAGEMENT (CRUD 7–10)
+    =========================== --}}
+    <li class="{{ (Str::startsWith($currentRoute, 'dashboard.crud-7') || Str::startsWith($currentRoute, 'dashboard.crud-8') || Str::startsWith($currentRoute, 'dashboard.crud-9') || Str::startsWith($currentRoute, 'dashboard.crud-10')) ? 'active open' : '' }}">
+      <a href="#" class="dropdown-toggle">
+        <i class="menu-icon fa fa-desktop"></i>
+        <span class="menu-text"> Blog Management </span>
+        <b class="arrow fa fa-angle-down"></b>
+      </a>
+
+      <b class="arrow"></b>
+
+      <ul class="submenu">
+        {{-- Category Section --}}
+        <li class="{{ (request()->routeIs('dashboard.crud-7.*') || request()->routeIs('dashboard.crud-8.*') || request()->routeIs('dashboard.crud-9.*')) ? 'active open' : '' }}">
+          <a href="#" class="dropdown-toggle">
+            <i class="menu-icon fa fa-caret-right"></i>
+            Categories
+            <b class="arrow fa fa-angle-down"></b>
+          </a>
+
+          <b class="arrow"></b>
+          <ul class="submenu">
+            <li class="{{ request()->routeIs('dashboard.crud-7.*') ? 'active' : '' }}">
+              <a href="{{ route('dashboard.crud-7.index') }}">
+                <i class="menu-icon fa fa-caret-right"></i> Category
+              </a>
+            </li>
+
+            <li class="{{ request()->routeIs('dashboard.crud-8.*') ? 'active' : '' }}">
+              <a href="{{ route('dashboard.crud-8.index') }}">
+                <i class="menu-icon fa fa-caret-right"></i> Sub-Category
+              </a>
+            </li>
+
+            <li class="{{ request()->routeIs('dashboard.crud-9.*') ? 'active' : '' }}">
+              <a href="{{ route('dashboard.crud-9.index') }}">
+                <i class="menu-icon fa fa-caret-right"></i> Sub-Sub-Category
+              </a>
+            </li>
+          </ul>
         </li>
 
-        {{-- CRUD Menu --}}
-        @php
-        $currentRoute = request()->route()->getName();
-        @endphp
-
-        <li class="{{ Str::startsWith($currentRoute, 'dashboard.crud') ? 'active open' : '' }}">
-            <a href="javascript:void(0);" class="dropdown-toggle">
-                <i class="menu-icon fa fa-list"></i>
-                <span class="menu-text"> CRUD </span>
-                <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-                {{-- <li class="{{ Str::startsWith($currentRoute, 'dashboard.crud-1') ? 'active' : '' }}">
-
-                  <a href="{{ route('dashboard.crud-1.index') }}">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    CRUD - 1
-                  </a>
-                </li> --}}
-                
-                <li class="{{ request()->routeIs('dashboard.crud-1.*') ? 'active' : '' }}">
-                  <a href="{{ route('dashboard.crud-1.index') }}">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    CRUD - 1
-                  </a>
-                </li>
-
-                <li class="{{ request()->routeIs('dashboard.crud-2.*') ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-2.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 2
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="{{ request()->routeIs('dashboard.crud-3.*') ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-3.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 3
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 4 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-4.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-4.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 4
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 5 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-5.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-5.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 5
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 6 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-6.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-6.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 6
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 7 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-7.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-7.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 7
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 8 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-8.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-8.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 8
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 9 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-9.*')  ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.crud-9.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        CRUD - 9
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                {{-- CRUD 10 --}}
-                <li class="{{ request()->routeIs('dashboard.crud-10.*') ? 'active' : '' }}">
-                  <a href="{{ route('dashboard.crud-10.index') }}">
-                    <i class="menu-icon fa fa-caret-right"></i>
-                    CRUD - 10
-                  </a>
-                </li>
-
-                {{-- Company --}}
-                <li class="{{ request()->routeIs('dashboard.company.*') ? 'active' : '' }}">
-                    <a href="{{ route('dashboard.company.index') }}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Company Settings
-                    </a>
-                </li>
-                
-            </ul>
+        {{-- Posts --}}
+        <li class="{{ request()->routeIs('dashboard.crud-10.*') ? 'active' : '' }}">
+          <a href="{{ route('dashboard.crud-10.index') }}">
+            <i class="menu-icon fa fa-caret-right"></i>
+            Posts Management
+          </a>
         </li>
+      </ul>
+    </li>
 
+    {{-- ==========================
+        OTHER PAGES SECTION
+    =========================== --}}
+    <li class="{{ Str::startsWith($currentRoute, 'dashboard.company') ? 'active open' : '' }}">
+      <a href="#" class="dropdown-toggle">
+        <i class="menu-icon fa fa-file-o"></i>
+        <span class="menu-text">
+          Other Pages
+          <span class="badge badge-primary">1</span>
+        </span>
+        <b class="arrow fa fa-angle-down"></b>
+      </a>
 
-        <li class="">
-            <a href="#" class="dropdown-toggle">
-                <i class="menu-icon fa fa-desktop"></i>
-                <span class="menu-text">
-                    CRUD Alt
-                </span>
+      <b class="arrow"></b>
 
-                <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-                <li class="">
-                    <a href="#" class="dropdown-toggle">
-                        <i class="menu-icon fa fa-caret-right"></i>
-
-                        Layouts
-                        <b class="arrow fa fa-angle-down"></b>
-                    </a>
-
-                    <b class="arrow"></b>
-
-                    <ul class="submenu">
-                        <li class="">
-                            <a href="top-menu.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Top Menu
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-
-                        <li class="">
-                            <a href="two-menu-1.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Two Menus 1
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-
-                        <li class="">
-                            <a href="two-menu-2.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Two Menus 2
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-
-                        <li class="">
-                            <a href="mobile-menu-1.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Default Mobile Menu
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-
-                        <li class="">
-                            <a href="mobile-menu-2.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Mobile Menu 2
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-
-                        <li class="">
-                            <a href="mobile-menu-3.html">
-                                <i class="menu-icon fa fa-caret-right"></i>
-                                Mobile Menu 3
-                            </a>
-
-                            <b class="arrow"></b>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="">
-                    <a href="typography.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Typography
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-            </ul>
+      <ul class="submenu">
+        <li class="{{ request()->routeIs('dashboard.company.*') ? 'active' : '' }}">
+          <a href="{{ route('dashboard.company.index') }}">
+            <i class="menu-icon fa fa-caret-right"></i>
+            Company Settings
+          </a>
         </li>
+      </ul>
+    </li>
+    </ul>
 
 
-
-        <li class="">
-            <a href="#" class="dropdown-toggle">
-                <i class="menu-icon fa fa-file-o"></i>
-
-                <span class="menu-text">
-                    Other Pages
-
-                    <span class="badge badge-primary">5</span>
-                </span>
-
-                <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-                <li class="">
-                    <a href="faq.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        FAQ
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="">
-                    <a href="error-404.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Error 404
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="">
-                    <a href="error-500.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Error 500
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="">
-                    <a href="grid.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Grid
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="">
-                    <a href="blank.html">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Blank Page
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-            </ul>
-        </li>
-    </ul><!-- /.nav-list -->
 
     <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
         <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
