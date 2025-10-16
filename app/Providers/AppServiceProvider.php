9cbc2648a20ec0ae738e73e3571b5947ce4c4f13
git-saif <saifse454@gmail.com>
2025-10-16 19:36:15 +0600

    create Global View Data Sharing for company settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Company;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        
+        // শুধু active কোম্পানি নেবে
+        $companySettings = Company::where('status', 'active')->first();
+
+        // সব Blade view-এ share করবে
+        View::share('companySettings', $companySettings);
     }
 }
