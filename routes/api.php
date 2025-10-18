5b63c329a1dcec1c48c3df3c349a2909b1c0d827
git-saif <saifse454@gmail.com>
2025-10-18 22:09:03 +0600

    create api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\Crud1Controller;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('dashboard')->group(function () {
+
+    Route::apiResources([
+        'crud-1' => Crud1Controller::class,
+        
+    ]);
 });
